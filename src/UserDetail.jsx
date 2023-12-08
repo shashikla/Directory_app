b87a10f1a0d9de235c6a1eb4721ab9b6aef420ab
unknown <shashikla.waghmare@htstechsolutions.com>
2023-12-08 18:44:37 +0530

    changes

--- src/UserDetail.jsx
+++ src/UserDetail.jsx
@@ -1,11 +1,69 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef  } from 'react';
 import { useParams, Link } from 'react-router-dom';
 import './directory.css';
+
+function Clock({ timezone, isPaused  }) {
+  const [currentTime, setCurrentTime] = useState('');
+
+  const formatDateTime = (dateTimeString) => {
+    const dateTime = new Date(dateTimeString);
+    const date = dateTime.toISOString().split('T')[0]; // Format date as YYYY-MM-DD
+    const time = dateTime.toTimeString().split(' ')[0]; // Format time as HH:MM:SS
+    return `${date} ${time}`;
+  };
+  const intervalRef = useRef(null);
+
+  useEffect(() => {
+    const fetchTime = async () => {
+      try {
+        const response = await fetch(`http://worldtimeapi.org/api/timezone/${timezone}`);
+        const data = await response.json();
+        const formattedDateTime = formatDateTime(data.datetime);
+        setCurrentTime(formattedDateTime);
+      } catch (error) {
+        console.error('Error fetching time:', error);
+      }
+    };
+
+    intervalRef.current = setInterval(() => {
+      if (!isPaused) {
+        fetchTime();
+      }
+    }, 1000);
+
+    fetchTime(); // Fetch time immediately when the component mounts
+
+    return () => clearInterval(intervalRef.current);
+  }, [timezone, isPaused]);
+
+  return <div>{currentTime}</div>;
+}
 
 function UserDetail() {
   const { id } = useParams();
   const [user, setUser] = useState(null);
   const [posts, setPosts] = useState([]);
+
+  const [countries, setCountries] = useState([]);
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [clockPaused, setClockPaused] = useState(false);
+
+  useEffect(() => {
+    // Fetch the list of countries and their timezones
+    fetch('http://worldtimeapi.org/api/timezone')
+      .then(response => response.json())
+      .then(data => setCountries(data))
+      .catch(error => console.error('Error fetching countries:', error));
+  }, []);
+
+  const handleCountryChange = (event) => {
+    setSelectedCountry(event.target.value);
+    setClockPaused(false);
+  };
+
+  const handlePauseResume = () => {
+    setClockPaused(prevState => !prevState);
+  };
 
   useEffect(() => {
 
@@ -33,7 +91,32 @@ function UserDetail() {
       gap:"10px",
       padding:"20px"
     }}>
+      <div className='header'>
       <Link to="/"><button className='btn-back'>Back</button></Link>
+      <div style={{
+          display:"flex",
+          gap:"10px"
+        }}>
+      <select value={selectedCountry} onChange={handleCountryChange} className='select-country'> 
+        {countries.map((country, index) => (
+          <option key={index} value={country}>
+            {country}
+          </option>
+        ))}
+      </select>
+      {selectedCountry && (
+        <div style={{
+          display:"flex",
+          gap:"10px"
+        }}>
+          <div className="clock_div">
+          <Clock timezone={selectedCountry}  isPaused={clockPaused}/>
+          </div>
+          <button onClick={handlePauseResume} className='resume-btn'>{clockPaused ? 'Resume' : 'Pause'}</button>
+        </div>
+      )}
+      </div>
+      </div>
       <h2 style={{textAlign:"center"}}>Profile Page</h2>
       <div className='row'>
         <div >
